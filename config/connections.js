c5c463b709346b982bb3bda41eebdc936d4f058d
Angelo Javonitalla <angelo.javonitalla@gmail.com>
2016-04-27 10:48:58 +0800

    Updating MONGOLAB variable name to match the default

--- config/connections.js
+++ config/connections.js
@@ -59,7 +59,7 @@ module.exports.connections = {
 
   mongolab: {
     adapter: 'sails-mongo',
-    url: process.env.MONGOLAB_URI
+    url: process.env.MONGODB_URI
   }
 
   /***************************************************************************
